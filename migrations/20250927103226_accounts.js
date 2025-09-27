0200df19e559de38c84cf66ce439c7a13341c7df
Jagadeesh2409 <jagadeesh242004@gmail.com>
2025-09-27 16:08:19 +0530

    knex config and created account table

---
+++ migrations/20250927103226_accounts.js
@@ -0,0 +1,15 @@
+exports.up = function(knex) {
+  return knex.schema.createTable('accounts', (table) => {
+    table.increments('id').primary();
+    table.string('name').notNullable();
+    table.string('email').notNullable().unique();
+     table.string('phno').notNullable();
+    table.string('password').notNullable();
+    table.string('role').defaultTo('client'); 
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('accounts'); 
+};
