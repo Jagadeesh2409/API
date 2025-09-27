ed64cf83126b91f6545d6f2e00b772de13c398e4
Jagadeesh2409 <jagadeesh242004@gmail.com>
2025-09-27 22:37:24 +0530

    created upload api

--- routes/auth.js
+++ routes/auth.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const {login,register,profile} = require('../controllers/authController')
 const authentication = require('../Middleware/authMiddleware')
+const upload = require('../Middleware/media')
 
 
-
+router.post('/upload',authentication,upload.single('media'),(req,res)=>res.status(200).json({message: "image uploaded succesfully"}))
 
 router.post('/register',register);
 
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const routes = require('./routes/auth')
+const path = require('path')
+app.use( express.static(path.join(__dirname, 'uploads')))
 
 app.use(express.json())
 
