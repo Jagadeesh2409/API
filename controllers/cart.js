1ae7c608a36b339eb4c9ac2aa088de0a80d92a8d
Jagadeesh2409 <jagadeesh242004@gmail.com>
2025-10-07 12:47:21 +0530

    alter

--- controllers/cart.js
+++ controllers/cart.js
@@ -1,13 +1,20 @@
 const knex = require("../db/knex");
 const addToCart = async (req, res) => {
   try {
+    console.log(req.user);
     const user_id = req.user.id;
     const { product_id, quantity } = req.body;
+
     if (!user_id || !product_id || !quantity) {
       return res.status(400).json({ message: "All fields are required" });
     }
+    const stock = await knex("products").where({ id: product_id }).first();
+    if (!stock.stock || stock.stock < quantity) {
+      return res.status(404).json({ message: "out of stock" });
+    }
 
     const cartItem = await knex("cart").where({ user_id, product_id }).first();
+
     if (cartItem) {
       await knex("cart")
         .where({ id: cartItem.id })
@@ -73,6 +80,9 @@ const getCart = async (req, res) => {
           "COALESCE(GROUP_CONCAT(products_img.url SEPARATOR ','), '') as images"
         )
       );
+    if (!cartItems) {
+      res.status(300).json({ cart: "cart is empty" });
+    }
 
     res.status(200).json({ cart: cartItems });
   } catch (error) {
@@ -81,4 +91,51 @@ const getCart = async (req, res) => {
   }
 };
 
-module.exports = { addToCart, removeFromCart, getCart };
+const emptyCart = async (req, res) => {
+  try {
+    const user_id = req.user.id;
+    if (!user_id) {
+      return res.status(400).json({ message: "User ID required" });
+    }
+    await knex("cart").where({ user_id }).del();
+    res.status(200).json({ message: "Cart emptied successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error", error });
+  }
+};
+
+const reduceQuantity = async (req, res) => {
+  try {
+    const { productId } = req.params;
+    const user_id = req.user.id;
+    if (!user_id) {
+      return res.status(400).json({ message: "User ID required" });
+    }
+    const cartItem = await knex("cart")
+      .where({ user_id, product_id: productId })
+      .first();
+    if (!cartItem) {
+      return res.status(404).json({ message: "Product not found in cart" });
+    }
+    if (cartItem.quantity > 1) {
+      await knex("cart")
+        .where({ id: cartItem.id })
+        .update({ quantity: cartItem.quantity - 1, updated_at: knex.fn.now() });
+      return res.status(200).json({ message: "Product quantity reduced by 1" });
+    }
+    await knex("cart").where({ id: cartItem.id }).del();
+    res.status(200).json({ message: "Product removed from cart" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error", error });
+  }
+};
+
+module.exports = {
+  addToCart,
+  removeFromCart,
+  getCart,
+  emptyCart,
+  reduceQuantity,
+};
