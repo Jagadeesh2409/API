50e0a0c9066e90e3afe006347e5fa0498249ab41
Jagadeesh2409 <jagadeesh242004@gmail.com>
2025-10-07 11:55:35 +0530

    changed

--- migrations/20250927103226_accounts.js
+++ migrations/20250927103226_accounts.js
@@ -1,15 +1,16 @@
-exports.up = function(knex) {
-  return knex.schema.createTable('accounts', (table) => {
-    table.increments('id').primary();
-    table.string('name').notNullable();
-    table.string('email').notNullable().unique();
-    table.string('phno')
-    table.string('password').notNullable();
-    table.string('role').defaultTo('client'); 
-    table.timestamp('created_at').defaultTo(knex.fn.now());
+exports.up = function (knex) {
+  return knex.schema.createTable("accounts", (table) => {
+    table.increments("id").primary();
+    table.string("name").notNullable();
+    table.string("email").notNullable().unique();
+    table.string("phno");
+    table.string("password").notNullable();
+    table.string("role").defaultTo("client");
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+    table.timestamp("updated_at").defaultTo(knex.fn.now());
   });
 };
 
-exports.down = function(knex) {
-  return knex.schema.dropTableIfExists('accounts'); 
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("accounts");
 };
