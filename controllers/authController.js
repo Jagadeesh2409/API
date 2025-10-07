43aa3af73c09e2f118e766429f6165b873cee5d2
Jagadeesh2409 <jagadeesh242004@gmail.com>
2025-10-07 13:14:15 +0530

    alter

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,56 +1,63 @@
-const jwt=require('jsonwebtoken')
-const bcrypt=require('bcrypt')
-const knex=require('../db/knex')
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const knex = require("../db/knex");
 
+const register = async (req, res) => {
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    return res.status(400).json({ message: "require all the fields" });
+  }
+  const existingUser = await knex("accounts").where({ email }).first();
+  if (existingUser) {
+    return res.status(400).json({ message: "user already exist" });
+  }
 
-const register = async(req,res)=>{
-    const {name,email,password} = req.body
-    if(!name||!email||!password){
-        return res.status(400).json({message:"require all the fields"})
+  const salt = parseInt(process.env.SALT_ROUND);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  await knex("accounts").insert({ name, email, password: hashedPassword });
+
+  return res.status(200).json({ message: "user successfully registered" });
+};
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({ message: "require all the fields" });
+  }
+  try {
+    const user = await knex("accounts").where({ email }).first();
+    if (!user) {
+      return res
+        .status(400)
+        .json({ message: "username or password incorrect" });
     }
-
-    const salt = parseInt(process.env.SALT_ROUND) 
-    const hashedPassword = await bcrypt.hash(password,salt)
-
-    await knex('accounts').insert({name,email,password:hashedPassword})
-
-    return res.status(200).json({message:"user successfully registered"})
- 
-}
-
-const login = async(req,res)=>{
-    const{email,password} = req.body
-     if(!email||!password){
-        return res.status(400).json({message:"require all the fields"})
+    const verify = bcrypt.compare(password, user.password);
+    if (!verify) {
+      return res
+        .status(400)
+        .json({ message: "username or password incorrect" });
     }
-    try{
-    const user = await knex('accounts').where({email}).first()
-    const verify = bcrypt.compare(password,user.password) 
-    if(!verify){
-        return res.status(400).json({message:"username or password incorrect"})
-    }
-    const token = jwt.sign({id:user.id,role:user.role},process.env.JWT_SECRET)
+    const token = jwt.sign(
+      { id: user.id, role: user.role },
+      process.env.JWT_SECRET
+    );
 
-    return res.status(200).json({message:"user succesfully login",token})
-    }
+    return res.status(200).json({ message: "user succesfully login", token });
+  } catch (err) {
+    return res.status(500).json({ message: "you haven't account" });
+  }
+};
 
-    catch(err){
-        return res.status(500).json({message:"you haven't account"})
-    }
-     
-}
+const profile = async (req, res) => {
+  try {
+    const id = req.user.id;
+    const data = await knex("accounts").where({ id });
 
-const profile = async (req,res) => {
-    try {
-        const id=req.user.id
-        const data =await knex('accounts').where({id})
-    return res.status(200).json({message:data})  
-    } catch (error) {
-        return res.status(200).json({message:error.message}) 
-    }
-}
+    return res.status(200).json({ message: data });
+  } catch (error) {
+    return res.status(200).json({ message: error.message });
+  }
+};
 
-
-module.exports={register,login,profile}
-
-
+module.exports = { register, login, profile };
